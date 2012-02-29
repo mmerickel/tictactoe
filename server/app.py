3ebdc5515daaca4c7df74da7b361de759abf3c5e
Domen Kožar <domen@dev.si>
2012-03-01 00:53:44 +0100

    add missing route

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ def make_app(global_conf, **settings):
     config.add_route('api.play', '/api/play')
     config.add_route('api.quit', '/api/quit')
     config.add_route('api.move', '/api/move')
+    config.add_route('api.chat', '/api/chat')
     config.add_route('api.updates', '/api/updates/{gameid}')
 
     config.scan('api')
